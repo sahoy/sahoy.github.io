b5ffb72e41e675bd11812cd1b188ecea5eaced45
sahoy <34254296+sahoy@users.noreply.github.com>
2019-02-04 14:41:37 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+width = document.body.clientWidth;
+height = document.body.clientHeight;
+
+var boardW = 10;
+var boardH = 20;
+
+var gridSpots = [];
+
+for(i = 0; i < boardW; i++){
+    var column = [];
+    for(j = 0; j < boardH; j++){
+        column.push(0);
+    }
+    gridSpots.push(column);
+}
+
+window.onload=function(){
+    document.getElementById('canvas').width = width;
+    document.getElementById('canvas').height = height;
+    c = document.getElementById('canvas').getContext('2d');
+    c.fillStyle='#297F29';
+    c.fillRect(0,0,width,height);
+    animate = setInterval(update,1000/frameRate);
+    document.addEventListener('mousemove',function(e){
+        
+	});
+}
