41059d12a6823ef4c8b7242153fa53d52107d2f0
sahoy <34254296+sahoy@users.noreply.github.com>
2019-02-27 18:42:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-"jennasawesomecode".length;
 width = document.body.clientWidth;
 height = document.body.clientHeight;
 
@@ -7,25 +6,29 @@ var boardH = 20;
 var frameNum = 0;
 var frameRate = 100;
 var gridSpots = [];
+var lines = 0;
 
 var tet1 = [[1, 1, 1, 1]];
-var tet2 = [[2, 2, 2], [2]];
-var tet3 = [[3], [3, 3, 3]];
-var tet4 = [[0, 7], [7, 7], [7]];
-var tet5 = [[6, 6, 6], [0, 6]];
-var tet6 = [[5], [5, 5], [0, 5]];
+var tet2 = [[2, 2, 2], [2, 0, 0]];
+var tet3 = [[3,0,0], [3, 3, 3]];
+var tet4 = [[0, 7], [7, 7], [7,0]];
+var tet5 = [[6, 6, 6], [0, 6, 0]];
+var tet6 = [[5,0], [5, 5], [0, 5]];
 var tet7 = [[4, 4], [4, 4]];
+var tets = [tet1,tet2,tet3,tet4,tet5,tet6,tet7];
+var fast = false;
 
 var tetrimino = [tet1,[0,0]];
+spawnRand();
 
 var colors = ['rgb(85, 85, 85)',
 							'rgb(81, 208, 255)',
-							'rgb(4, 68, 216)',
+							'rgb(64, 109, 214)',
 							'rgb(252, 187, 65)',
 							'rgb(247, 240, 34)',
-							'rgb(88, 216, 86)',
+							'rgb(131, 255, 107)',
 							'rgb(157, 76, 211)',
-							'rgb(191, 28, 28)'];
+							'rgb(244, 66, 66)'];
 
 for (i = 0; i < boardW; i++) {
 	var column = [];
@@ -34,7 +37,6 @@ for (i = 0; i < boardW; i++) {
 	}
 	gridSpots.push(column);
 }
-//gridSpots[boardW-1][boardH-1]=1;
 
 window.onload = function () {
 	document.getElementById('canvas').width = width;
@@ -43,27 +45,54 @@ window.onload = function () {
 	c.fillStyle = 'rgb(55,55,55)';
 	c.fillRect(0, 0, width, height);
 	animate = setInterval(update, 1000 / frameRate);
+	document.onkeyup = function(e){
+		switch (e.keyCode) {
+				case 40:
+						fast = false;
+						frameNum = 0;
+						break;
+		}
+	} 
 	document.onkeydown = function(e) {
     switch (e.keyCode) {
         case 37:
-						if(tetrimino[1][0]>-1*Math.floor(boardW / 2 - 1)){
+						if(tetrimino[1][0]>0){
 							eraseTet(tetrimino[0],tetrimino[1][0],tetrimino[1][1]);
-							tetrimino[1][0]-=1;
+							if(collision([tetrimino[0],[tetrimino[1][0]-1,tetrimino[1][1]]]) === false){
+								tetrimino[1][0] -= 1;
+							}
 							deployTet(tetrimino[0],tetrimino[1][0],tetrimino[1][1]);
 							display();
 						}
             break;
         case 39:
-            if(tetrimino[1][0]<Math.floor(boardW / 2 - 1)+2-tetrimino[0].length){
+            if(tetrimino[1][0]< boardW - tetrimino[0].length){
 							eraseTet(tetrimino[0],tetrimino[1][0],tetrimino[1][1]);
-							tetrimino[1][0]+=1;
+							if(collision([tetrimino[0],[tetrimino[1][0]+1,tetrimino[1][1]]]) === false){
+								tetrimino[1][0] += 1;
+							}
 							deployTet(tetrimino[0],tetrimino[1][0],tetrimino[1][1]);
 							display();
 						}
             break;
-        case 40:
-            alert('down');
+        case 90:
+						eraseTet(tetrimino[0],tetrimino[1][0],tetrimino[1][1]);
+						if(collision(rotate(tetrimino)) == false){
+							tetrimino = rotate(tetrimino);
+						}
+						deployTet(tetrimino[0],tetrimino[1][0],tetrimino[1][1]);
+						display();
             break;
+				case 88:
+						eraseTet(tetrimino[0],tetrimino[1][0],tetrimino[1][1]);
+						if(collision(rotate_b(tetrimino)) == false){
+							tetrimino = rotate_b(tetrimino);
+						}
+						deployTet(tetrimino[0],tetrimino[1][0],tetrimino[1][1]);
+						display();
+            break;
+				case 40:
+						fast = true;
     }
 };
 }
@@ -79,6 +108,8 @@ function lowest_nonzero(arr){
 }
 
 function display() {
+	c.fillStyle = 'rgb(55,55,55)';
+	c.fillRect(0, 0, width, height);
 	tileW = Math.min(height / boardH, width / boardW);
 	startX = (width - tileW * boardW) / 2;
 	for (i = 0; i < boardW; i++) {
@@ -93,7 +124,7 @@ function deployTet(tet,xi,yi) {
 	for (x = 0; x < tet.length; x++) {
 		for (y = 0; y < tet[x].length; y++) {
 			if(tet[x][y]!=0){
-				gridSpots[Math.floor(x + boardW / 2 - 1)+xi][y+yi] = tet[x][y];
+				gridSpots[x+xi][y+yi] = tet[x][y];
 			}
 		}
 	}
@@ -102,13 +133,69 @@ function deployTet(tet,xi,yi) {
 function eraseTet(tet,xi,yi){
 	for (x = 0; x < tet.length; x++) {
 		for (y = 0; y < tet[x].length; y++) {
-			gridSpots[Math.floor(x + boardW / 2 - 1)+xi][y+yi] = 0;
+			if(tet[x][y]!=0){
+				gridSpots[x+xi][y+yi] = 0;
+			}
 		}
 	}
 }
 
-function spawnRand(){
+function collision(tet){
+	for(tetx = 0; tetx < tet[0].length; tetx++){
+		for(tety = 0; tety < tet[0][tetx].length; tety++){
+			if(tet[0][tetx][tety] !== 0 && gridSpots[tetx+tet[1][0]][tety+tet[1][1]] !== 0){
+				return true;
+			}
+		}
+	}
+	return false;
+}
 
+function spawnRand(){
+	tetrimino[1]=[Math.floor(boardW / 2 - 1),-1];
+	tetrimino[0]=tets[Math.floor(7*Math.random())];
+}
+
+function rotate(tet){
+	new_tet = []
+	for(ytet = 0; ytet < tet[0][0].length; ytet++){
+		new_tet.push([])
+		for(xtet = 0; xtet < tet[0].length; xtet++){
+			new_tet[ytet].unshift(tet[0][xtet][ytet]);
+		}
+	}
+	return [new_tet,tet[1]];
+}
+
+function rotate_b(tet){
+	new_tet = []
+	for(ytet = 0; ytet < tet[0][0].length; ytet++){
+		new_tet.unshift([])
+		for(xtet = 0; xtet < tet[0].length; xtet++){
+			new_tet[0].push(tet[0][xtet][ytet]);
+		}
+	}
+	
+	return [new_tet,tet[1]];
+}
+
+function clearLines(){
+	for(gy=0;gy<boardH;gy++){
+		var full = true;
+		for(gx=0;gx<boardW;gx++){
+			if(gridSpots[gx][gy]===0){
+				full = false;
+			}
+		}
+		if(full){
+			lines+=1;
+			for(cy=gy;cy>0;cy--){
+				for(cx=0;cx<boardW;cx++){
+					gridSpots[cx][cy]=gridSpots[cx][cy-1];
+				}
+			}
+		}
+	}
 }
 
 function update() {
@@ -122,14 +209,14 @@ function update() {
 			if(tetrimino[0][x][y]!=0){
 				if(y+tetrimino[1][1]==boardH-1){
 						deployTet(tetrimino[0],tetrimino[1][0],tetrimino[1][1]);
-						tetrimino[0]=tet2;
-						tetrimino[1]=[0,0];
+						clearLines();
+						spawnRand();
 						flag = true;
 					}
-				else if(gridSpots[tetrimino[1][0]+Math.floor(x + boardW / 2 - 1)][y+tetrimino[1][1]+1]!==0){
+				else if(gridSpots[tetrimino[1][0]+x][y+tetrimino[1][1]+1]!==0){
 						deployTet(tetrimino[0],tetrimino[1][0],tetrimino[1][1]);
-						tetrimino[0]=tet2;
-						tetrimino[1]=[0,0];
+						clearLines();
+						spawnRand();
 						flag = true;
 					}
 			}
@@ -138,8 +225,21 @@ function update() {
 			}
 		}
 		tetrimino[1][1]+=1;
+		if(collision(tetrimino)){
+			clearInterval(animate);
+			return 0;
+		}
 	}
 	deployTet(tetrimino[0],tetrimino[1][0],tetrimino[1][1]);
 	display();
-	frameNum=(frameNum+1)%frameRate;
+
+	c.fillStyle = 'white';
+	c.font = width/18.5 + "px Pixelated"
+	c.fillText("LINES:" + lines,width/25,width/10);
+
+	speed = 16;
+	if(fast){
+		speed = 5120;
+	}
+	frameNum=(frameNum+1)%(frameRate/speed);
 }
